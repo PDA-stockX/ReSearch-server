a78fd91a39a962a403f70d35f85230b3196b7cd4
kim hyun soo <hyjkl0124@naver.com>
2024-03-12 10:08:27 +0900

    feat io follow Analyst

--- models/like.js
+++ models/like.js
@@ -1,27 +1,67 @@
-'use strict';
-const {
-    Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class Like extends Model {
-        static associate(models) {
-            // define association here
-            this.belongsTo(models.User, {
-                as: 'user',
-                foreignKey: 'userId',
-                onDelete: 'CASCADE'
-            });
-            this.belongsTo(models.Report, {
-                as: 'report',
-                foreignKey: 'reportId',
-                onDelete: 'CASCADE'
-            });
-        }
+  class Like extends Model {
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, {
+        as: "user",
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
+      this.belongsTo(models.Report, {
+        as: "report",
+        foreignKey: "reportId",
+        onDelete: "CASCADE",
+      });
     }
 
-    Like.init({}, {
-        sequelize,
-        modelName: 'Like',
-    });
-    return Like;
+    static async pressLike(userId, analId) {
+      try {
+        const analLike = await this.create({
+          userId,
+          analId,
+        });
+        const analLikes = await this.findAll({
+          userId: userId,
+          analId: analId,
+        });
+        return {
+          userId: analLike.userId,
+          analId: analLike.analId,
+          likeNum: analLikes.length,
+        };
+      } catch (err) {
+        throw err;
+      }
+    }
+
+    static async pressUnLike(userId, analId) {
+      try {
+        const analUnLike = await this.destory({
+          where: { userId: userId, analId: analId },
+        });
+        const analLikes = await this.findAll({
+          userId: userId,
+          analId: analId,
+        });
+        return {
+          userId: analUnLike.userId,
+          analId: analUnLike.analId,
+          likeNum: analLikes.length,
+        };
+      } catch (err) {
+        throw err;
+      }
+    }
+  }
+
+  Like.init(
+    {},
+    {
+      sequelize,
+      modelName: "Like",
+    }
+  );
+  return Like;
 };
--- models/user.js
+++ models/user.js
@@ -1,77 +1,79 @@
-const {isEmail} = require('validator');
-const bcrypt = require('bcrypt');
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
-const {
-    Model
-} = require('sequelize');
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
-        static associate(models) {
-            // define association here
-            this.hasMany(models.Follow, {
-                as: 'follows',
-                foreignKey: 'userId',
-                onDelete: 'CASCADE'
-            });
-        }
-
-        static async signUp(email, password,name, nickname){
-            const salt = await bcrypt.genSalt();
-            console.log(salt);
-
-            try {
-                const hashedPassword = await bcrypt.hash(password, salt);
-                const user = await this.create({
-                    email,
-                    password : hashedPassword,
-                    name,
-                    nickname,
-                });
-                return {
-                    id: user.id,
-                    email: user.email,
-                    name : user.name,
-                    nickname : user.nickname,
-                };
-            } catch (err) {
-                throw err;
-            }
-        };
-
-        static async signIn (email, password) {
-            const user = await this.findOne({where: {email}});
-            if (user) {
-                const auth = await bcrypt.compare(password,user.password);
-                if (auth) {
-                    return user.visibleUser;
-                } throw Error("잘못된 비밀번호");
-            } throw Error("잘못된 이메일");
-        };
-
+  class User extends Model {
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Follow, {
+        as: "follows",
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
     }
 
-    User.init({
-        name: DataTypes.STRING,
-        nickname: DataTypes.STRING,
-        password: DataTypes.STRING,
-        email: DataTypes.STRING,
-        visibleUser: {
-            type: DataTypes.VIRTUAL,
-            get() {
-                return {
-                    id: this.id,
-                    email: this.email,
-                    name: this.name,
-                    nickname: this.nickname,
-                }
-            },
-            set() {
-                throw new Error('가상 필드입니다. 이 값을 설정할 수 없습니다.');
-            }
+    static async signUp(email, password, name, nickname) {
+      const salt = await bcrypt.genSalt();
+      console.log(salt);
+
+      try {
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = await this.create({
+          email,
+          password: hashedPassword,
+          name,
+          nickname,
+        });
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          nickname: user.nickname,
+        };
+      } catch (err) {
+        throw err;
+      }
+    }
+
+    static async signIn(email, password) {
+      const user = await this.findOne({ where: { email } });
+      if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+          return user.visibleUser;
         }
-    }, {
-        sequelize,
-        modelName: 'User',
-    });
-    return User;
+        throw Error("잘못된 비밀번호");
+      }
+      throw Error("잘못된 이메일");
+    }
+  }
+
+  User.init(
+    {
+      name: DataTypes.STRING,
+      nickname: DataTypes.STRING,
+      password: DataTypes.STRING,
+      email: DataTypes.STRING,
+      visibleUser: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return {
+            id: this.id,
+            email: this.email,
+            name: this.name,
+            nickname: this.nickname,
+          };
+        },
+        set() {
+          throw new Error("가상 필드입니다. 이 값을 설정할 수 없습니다.");
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
 };
