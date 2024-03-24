d0466aed3a801a60fb61c7537d8a022fb7f32c63
yerin <pyr53540@gmail.com>
2024-03-25 00:23:49 +0900

    feat: getFirmRankings

--- routes/firms.js
+++ routes/firms.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {initModels} = require('../models/initModels');
-const {Op} = require("sequelize");
+const { initModels } = require('../models/initModels');
+const { Op } = require("sequelize");
 
 const models = initModels();
 
@@ -18,9 +18,29 @@ router.get('/search', async (req, res, next) => {
         res.json(firms);
     } catch (err) {
         console.error(err);
-        res.status(400).json({message: "fail"});
+        res.status(400).json({ message: "fail" });
         next(err);
     }
 });
+
+async function getFirmRankings(orderBy, res) {
+    try {
+        // Firm 테이블에서 name, returnRate, achievementScore 가져오기
+        const FirmData = await models.Firm.findAll({
+            attributes: ["id", "name", "returnRate", "achievementScore"]
+        });
+        // res.send(FirmData);
+
+        // Firm 기준으로 정렬
+        const sortedFirmRankings = sectorData.sort(
+            (a, b) => b[orderBy] - a[orderBy]
+        );
+
+        res.json(sortedFirmRankings);
+    } catch (err) {
+        console.error(`Error retrieving Firm data (${orderBy}):`, err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
 
 module.exports = router;
